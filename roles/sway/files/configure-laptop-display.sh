b32748306d3415219a357ccbc794e65baa883e3b
RolandWarburton <warburtonroland@gmail.com>
2022-12-19 22:40:01 +1100

    Modify configure-laptop-display to set background

--- roles/sway/files/configure-laptop-display.sh
+++ roles/sway/files/configure-laptop-display.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/bash
-
-# get the number of displays
-displays=$(swaymsg -t get_outputs | grep name | wc -l)
-
-# the laptop display
 LAPTOP="eDP-1";
-
-# if there is more than one display and the lid is closed
+displays=$(swaymsg -t get_outputs | grep name | wc -l)
 if [[ $displays -gt 1 ]] && $(grep -q closed /proc/acpi/button/lid/LID/state); then
+  echo "disabling laptop"
   swaymsg output "$LAPTOP" disable
 else
+  echo "enabling laptop"
   swaymsg output "$LAPTOP" enable
 fi;
-fi
+
+# try and start swww
+swww init &> /dev/null
+
+# this is not used at the moment
+status=$?
+
+# set the background
+swww img $HOME/.local/media/background.png;
+
+# TODO: this is not tested and i dont think its working correctly
+# if [ -n "$SWAY_ENABLE_DESKTOP_BACKGROUND" ]; then
+#   swww img $HOME/.local/media/background.png;
+# fi
