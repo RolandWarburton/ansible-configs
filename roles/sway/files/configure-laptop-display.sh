e6aacf3d4171708969b02e3bcfaa474f86947bb3
RolandWarburton <warburtonroland@gmail.com>
2022-12-19 18:18:39 +1100

    Update configure-laptop-display.sh

--- roles/sway/files/configure-laptop-display.sh
+++ roles/sway/files/configure-laptop-display.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
+
+# get the number of displays
+displays=$(swaymsg -t get_outputs | grep name | wc -l)
+
+# the laptop display
 LAPTOP="eDP-1";
-if [ $(grep -q open /proc/acpi/button/lid/LID/state) -a $(lsmod | grep -q i915) ]; then
-  swaymsg output "$LAPTOP" enable
-else
+
+# if there is more than one display and the lid is closed
+if [[ $displays -gt 1 ]] && $(grep -q closed /proc/acpi/button/lid/LID/state); then
   swaymsg output "$LAPTOP" disable
+else
+  swaymsg output "$LAPTOP" enable
+fi;
 fi
