0a5852072dd136fd4a74bb86559cd499526c0117
RolandWarburton <warburtonroland@gmail.com>
2024-12-03 20:20:35 +1100

    Vibing with go role

--- roles/golang/files/install-golangci-lint.zsh
+++ roles/golang/files/install-golangci-lint.zsh
@@ -4,4 +4,4 @@ source $HOME/.zshrc
 
 curl -sSfL \
   https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | \
-  sh -s -- -b $(go env GOROOT)/bin v1.46.2
+  sh -s -- -b $(go env GOROOT)/bin v1.62.0
