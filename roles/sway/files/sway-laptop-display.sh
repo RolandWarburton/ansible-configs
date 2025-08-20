be5612660cef9bdf9f1523f481f2fb2a984bb0b8
RolandWarburton <warburtonroland@gmail.com>
2025-08-21 08:49:11 +1000

    Update swww

--- roles/sway/files/sway-laptop-display.sh
+++ roles/sway/files/sway-laptop-display.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 LAPTOP="eDP-1";
+WAYLAND_DISPLAY="wayland-1"
+
 displays=$(swaymsg -t get_outputs | grep name | wc -l)
 if [[ $displays -gt 1 ]] && $(grep -q closed /proc/acpi/button/lid/LID/state); then
   swaymsg output "$LAPTOP" disable
@@ -9,14 +11,11 @@ fi;
 
 if [ -n "$SWAY_ENABLE_DESKTOP_BACKGROUND" ]; then
   # Check if swww is not running
-  if ! pgrep swww >/dev/null 2>&1; then
+  if ! pgrep swww >/dev/null 3>&1; then
     # try and start swww
-    swww init &> /dev/null
-
-    # this is not used at the moment
-    status=$?
-
-    # set the background
-    swww img $HOME/.local/media/background.png;
+    swww-daemon &> /dev/null
+    sleep 1
   fi
+  # set the background
+  swww img $HOME/.local/media/background.png;
 fi
