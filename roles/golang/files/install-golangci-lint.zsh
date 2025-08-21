e761799fcda10b73e2bcdfb9b476944ccf7cec05
RolandWarburton <warburtonroland@gmail.com>
2025-08-22 00:34:03 +1000

    Refactor golang role

--- roles/golang/files/install-golangci-lint.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-source $HOME/.zshrc
-
-curl -sSfL \
-  https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | \
-  sh -s -- -b $(go env GOROOT)/bin v1.62.0
